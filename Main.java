7246209551f02fedee87f0a3b613694136908cfa
chloetse <43720148+chloetse@users.noreply.github.com>
2021-03-30 11:47:35 +0800

    Create Main.java
    This is a mere draft of the Main.java class. Currently, this class will be ignored because the main method is in the Game.java file. Afterward, the main method will reside here.

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+/*
+
+public class Main {
+
+    //Main method
+    public static void main (String[] args) {
+        Scanner inputS = new Scanner(System.in);
+        Game gameOne = new Game();
+    }
+}
+
+ */
