8c4a60a48d0d83414efc78fe36ddc30e6b23fa1d
chloetse <43720148+chloetse@users.noreply.github.com>
2021-04-01 00:00:10 +0800

    Update Game.java
    This modified method carries out the game perfectly.
    Next steps:
    - Maze.java inheritance
    - Remove main class to Main.java

--- Game.java
+++ Game.java
@@ -24,26 +24,27 @@ public class Game {
         myBoard.hitOrMiss();
     }
 
-    //This method ask a question (that requires a String input) and then returns the String input
-    private static String getValidStringInput(Scanner inputS, String question) {
-        System.out.print(question + " "); //prints the question the user must respond to
-        return inputS.nextLine(); //returns the String input
-    }
-
     //main method
     public static void main(String[] args) {
         Game myGame = new Game();
         Scanner inputS = new Scanner(System.in);
         String getStatistics = getValidStringInput(inputS, "Do you want to see the statistics for the game? " +
                 "Type 'y' for yes, and another other key for no.");
-        if (getStatistics.equals("y")) {
-            myGame.myBoard.statistics();
-        }
         myGame.intro();
         while (myGame.myBoard.numberOfSuccessfulMissiles < 16) {
             myGame.playingGame(inputS);
+            if (getStatistics.equals("y")) {
+                myGame.myBoard.statistics();
+            }
         }
         System.out.println("Congratulations, you have won Battleship!");
         System.exit(0);
     }
+
+    //This method ask a question (that requires a String input) and then returns the String input
+    private static String getValidStringInput(Scanner inputS, String question) {
+        System.out.print(question + " "); //prints the question the user must respond to
+        return inputS.nextLine(); //returns the String input
+    }
+
 }
