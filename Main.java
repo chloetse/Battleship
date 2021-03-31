27f67fd8525c634b22ffbacc1b4ca293e866d620
chloetse <43720148+chloetse@users.noreply.github.com>
2021-04-01 00:05:39 +0800

    Update Main.java
    Added the main method

--- Main.java
+++ Main.java
@@ -1,14 +1,27 @@
 import java.util.*;
 
-/*
-
 public class Main {
 
-    //Main method
-    public static void main (String[] args) {
+    //main method
+    public static void main(String[] args) {
+        Game myGame = new Game();
         Scanner inputS = new Scanner(System.in);
-        Game gameOne = new Game();
+        String getStatistics = getValidStringInput(inputS, "Do you want to see the statistics for the game? " +
+                "Type 'y' for yes, and another other key for no.");
+        myGame.intro();
+        while (myGame.myBoard.numberOfSuccessfulMissiles < 16) {
+            myGame.playingGame(inputS);
+            if (getStatistics.equals("y")) {
+                myGame.myBoard.statistics();
+            }
+        }
+        System.out.println("Congratulations, you have won Battleship!");
+        System.exit(0);
+    }
+
+    //This method ask a question (that requires a String input) and then returns the String input
+    private static String getValidStringInput(Scanner inputS, String question) {
+        System.out.print(question + " "); //prints the question the user must respond to
+        return inputS.nextLine(); //returns the String input
     }
 }
-
- */
