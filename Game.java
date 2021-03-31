b99620343e56c8db02aea1abe9b9632ef883cba2
chloetse <43720148+chloetse@users.noreply.github.com>
2021-03-31 23:32:18 +0800

    Update Game.java
    Changes:
    - Added the hit or miss method
    - Resolved the error with the x-coordinate and y-coordinate
    - Keeps asking the user for their next coordinates for as long as the user does not win
    - Added the statistics method

--- Game.java
+++ Game.java
@@ -10,13 +10,6 @@ public class Game {
         myBoard = new Board();
     }
 
-    public void userMove(Scanner inputS) {
-        int xCoordinate = getValidIntegerInputInRange(inputS, "Choose your x-coordinate:", 0, 10);
-        int yCoordinate = getValidIntegerInputInRange(inputS, "Choose your y-coordinate:", 0, 10);
-        System.out.println("The coordinates you have chosen are: (" + xCoordinate + ", " + yCoordinate + ")");
-        myBoard.numberOfMissilesFired++;
-    }
-
     public void intro() {
         System.out.println("Welcome to Battleships!");
         System.out.println("User board: ");
@@ -25,36 +18,32 @@ public class Game {
         myBoard.printSolution();
     }
 
-    /* asks a question that requires an integer input and then checks the validity of the input
-   if the input is not a valid integer (eg. String, char), the program will not continue
-   (hence, the while loop) until a valid integer is inputted
-   this prevents the program from continuing with an incorrect input
-   */
-    private int getValidIntegerInput(Scanner inputS, String question) {
+
+    public void playingGame(Scanner inputS) {
+        myBoard.userMove(inputS);
+        myBoard.hitOrMiss();
+    }
+
+    //This method ask a question (that requires a String input) and then returns the String input
+    private static String getValidStringInput(Scanner inputS, String question) {
         System.out.print(question + " "); //prints the question the user must respond to
-        while (!inputS.hasNextInt()) { //while the input is not a valid integer
-            System.err.println("Error: not a valid integer input"); //print the error message of "invalid input"
-            inputS.nextLine(); //keep waiting for input
-        }
-        return Integer.parseInt(inputS.nextLine()); //returns the valid input (may be set to a variable)
+        return inputS.nextLine(); //returns the String input
     }
 
-    //same as getValidIntegerInput, but includes the minimum and maximum  value
-    private int getValidIntegerInputInRange(Scanner inputS, String question, int minValue, int maxValue) {
-        while (true) {
-            int userInputValue = getValidIntegerInput(inputS, question);
-            if (userInputValue < minValue || userInputValue > maxValue) {
-                System.err.println("Error: input not within the valid range"); //prints the error message of "invalid input"
-            } else {
-                return userInputValue;
-            }
-        }
-
-    }
-
-    //temporary main method
+    //main method
     public static void main(String[] args) {
         Game myGame = new Game();
+        Scanner inputS = new Scanner(System.in);
+        String getStatistics = getValidStringInput(inputS, "Do you want to see the statistics for the game? " +
+                "Type 'y' for yes, and another other key for no.");
+        if (getStatistics.equals("y")) {
+            myGame.myBoard.statistics();
+        }
         myGame.intro();
+        while (myGame.myBoard.numberOfSuccessfulMissiles < 16) {
+            myGame.playingGame(inputS);
+        }
+        System.out.println("Congratulations, you have won Battleship!");
+        System.exit(0);
     }
 }
