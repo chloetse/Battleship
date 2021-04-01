cfca7fe5cf0c7be839e9656edb74be7b3df007a6
chloetse <43720148+chloetse@users.noreply.github.com>
2021-04-01 09:52:23 +0800

    Update Destroyers.java
    The final version of Destroyers.java with comments added for readability.

--- Destroyers.java
+++ Destroyers.java
@@ -1,5 +1,6 @@
+//Destroyer is the child class, Ship is the parent class
 public class Destroyers extends Ship {
     public Destroyers() {
         super('D', 2);
-    }
+    } //symbol: D, size: 2
 }
