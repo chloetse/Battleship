8f6f2cd2ca020f804c8f5c0c08795c9bafc44545
chloetse <43720148+chloetse@users.noreply.github.com>
2021-04-01 09:51:12 +0800

    Update Battleship.java
    The final version of Battleship.java with comments added for readability.

--- Battleship.java
+++ Battleship.java
@@ -1,5 +1,6 @@
+//Battleship is the child class, Ship is the parent class
 public class Battleship extends Ship {
     public Battleship() {
-        super('B', 4);
+        super('B', 4); //symbol: B, size: 4
     }
 }
