b2dae41708f0e5351b23591826cd1495c7f16b29
chloetse <43720148+chloetse@users.noreply.github.com>
2021-04-01 09:53:29 +0800

    Update Main.java
    The final version of Main.java with comments added for readability.

--- Main.java
+++ Main.java
@@ -1,25 +1,27 @@
-import java.util.*;
+import java.util.*; //import statement
 
 public class Main {
 
     //main method
     public static void main(String[] args) {
         Game myGame = new Game();
-        Scanner inputS = new Scanner(System.in);
+        Scanner inputS = new Scanner(System.in); //Scanner
+        //asks the user whether they want to see game statistics
         String getStatistics = getValidStringInput(inputS, "Do you want to see the statistics for the game? " +
                 "Type 'y' for yes, and another other key for no.");
-        myGame.intro();
+        myGame.intro(); //calls the introduction method
+        //keeps asking the user for their next move until they have hit all parts of the ship
         while (myGame.myBoard.numberOfSuccessfulMissiles < 16) {
-            myGame.playingGame(inputS);
+            myGame.playingGame(inputS); //calls the playing game method
             if (getStatistics.equals("y")) {
-                myGame.myBoard.statistics();
+                myGame.myBoard.statistics(); //calls the statistics method
             }
         }
-        System.out.println("Congratulations, you have won Battleship!");
-        System.exit(0);
+        System.out.println("Congratulations, you have won Battleship!"); //end statement
+        System.exit(0); //exits the program
     }
 
-    //This method ask a question (that requires a String input) and then returns the String input
+    //asks a question (that requires a String input) and then returns the String input
     private static String getValidStringInput(Scanner inputS, String question) {
         System.out.print(question + " "); //prints the question the user must respond to
         return inputS.nextLine(); //returns the String input
