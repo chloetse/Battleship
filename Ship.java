6e11f0afe7b63639e115e1aa407f4290bd4c3137
chloetse <43720148+chloetse@users.noreply.github.com>
2021-04-01 09:50:06 +0800

    Update Ship.java
    The final version of Ship.java with comments added for readability.

--- Ship.java
+++ Ship.java
@@ -1,31 +1,60 @@
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.ThreadLocalRandom; //import statement
 
 public class Ship {
 
     //variables
     char symbol;
     int size;
-    int bound;
 
     //constructor
     public Ship(char mySymbol, int mySize) {
         symbol = mySymbol;
         size = mySize;
-        bound = 10 - size + 1;
     }
 
-    public void placeOnBoard(char[][] b) {
+    //places the ships on the board randomly, either vertically or horizontally
+    public void placeOnBoard(char[][] board) {
+        //randomly chooses whether the ship should be placed horizontally or vertically
+        int orientation = ThreadLocalRandom.current().nextInt(0, 1);
 
-        int randomInt = getRand();
-        for (int i = 0; i < size; i++) {
-            while (b[randomInt][randomInt + i] != '.') {
-                randomInt = getRand();
+        //boundary of the board
+        int xBound = board.length - 1;
+        int yBound = board[0].length - 1;
+
+        while (true) {
+            //generates random coordinates
+            int x = ThreadLocalRandom.current().nextInt(0, xBound);
+            int y = ThreadLocalRandom.current().nextInt(0, yBound);
+
+            //places horizontally
+            if (orientation == 0 && x + size < xBound) {
+                if (checkAndPlaceOnBoard(board, x, y, 1, 0)) {
+                    break;
+                }
             }
-            b[randomInt][randomInt + i] = symbol;
+
+            //places vertically
+            else if (y + size < yBound) {
+                if (checkAndPlaceOnBoard(board, x, y, 0, 1)) {
+                    break;
+                }
+            }
         }
     }
 
-    private int getRand() {
-        return ThreadLocalRandom.current().nextInt(size, bound);
+    //checks whether the ships can be placed on the board, and places the ships if allowed
+    public boolean checkAndPlaceOnBoard(char[][] b, int x, int y, int dx, int dy) {
+        //checks to ensure that the ship can be placed
+        for (int i = 0; i < size; i++) {
+            if (b[x + dx * i][y + dy * i] != '.') {
+                return false; //cannot place the ship
+            }
+        }
+
+        //updates the solution board
+        for (int i = 0; i < size; i++) {
+            b[x + dx * i][y + dy * i] = symbol;
+        }
+        return true;
     }
 }
